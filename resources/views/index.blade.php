c778add13fb38943b34f2a2e70ce4677d3d3e049
LeggyBoiTim <105779117+LeggyBoiTim@users.noreply.github.com>
2026-04-21 12:13:08 +0200

    Vue Router, folder structure

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Book-collection</title>
-        @vite(['resources/css/app.css', 'resources/js/app.ts'])
+        @vite(['resources/css/app.css', 'resources/js/main.ts'])
     </head>
     <body>
         <div id="app"></div>
